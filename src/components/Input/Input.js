18f66a391ee22cd7dec86c0c0fd33b728db18c7e
Atul Sharma <theatsharma@gmail.com>
2018-11-05 02:33:16 +0530

    added input text and number component

---
+++ src/components/Input/Input.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Input.css';
+
+export default class Input extends Component {
+    static propTypes = {
+        inputType: PropTypes.oneOf(['text', 'number']).isRequired,
+        label: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        handleInputChange: PropTypes.func.isRequired,
+        value: PropTypes.oneOfType([
+            PropTypes.string,
+            PropTypes.number,
+        ]).isRequired,
+        placeholder: PropTypes.string,
+    };
+    
+    render() {
+        return (
+            <div className="form-group">
+                <label className="form-label">{this.props.label}</label>
+                <input
+                className="form-input"
+                name={this.props.name}
+                type={this.props.inputType}
+                value={this.props.value}
+                onChange={this.props.handleInputChange}
+                placeholder={this.props.placeholder} />
+            </div>
+        )
+    }
+}
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import Input from '../../components/Input/Input';
+
+const flexiConfig = {
+	items: [
+		{
+			'name': 'username',
+			'label': 'Full Name',
+			'placeholder': 'Enter your name',
+			'type': 'text',
+		},
+		{
+			'name': 'usercontact_no',
+			'label': 'Mobile No',
+			'placeholder': 'Enter your mobile no',
+			'type': 'number',
+		},
+		{
+			'name': 'states',
+			'label': 'states',
+			'type': 'dropdown',
+			'values': [
+				'Maharashtra',
+				'Kerala',
+				'Tamil Nadu',
+			]
+		}
+	]
+};
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			inputValue: '',
+		}
+	}
+
+	handleInputChange = e => {
+		this.setState({ inputValue: e.target.value }, () => console.log('name:', this.state.inputValue));
+	}
+
 	render() {
 		return (
-			<div>
-				HELLO
+			<div className="container">
+				{flexiConfig.items.map((item) => 
+					<Fragment>
+						{ (item.type === 'text' || item.type === 'number') && <Input 
+							inputType={item.type}
+							label={item.label}
+							name={item.name}
+							handleInputChange={this.handleInputChange}
+							value={"name"}
+							placeholder={item.placeholder}
+						/>}	
+					</Fragment>	
+					)	
+				}
 			</div>
 		);
 	}
